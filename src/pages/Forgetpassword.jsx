bda642e4b6f50c8d9d34a18b7aebaaad1b52ebe6
11Aravind <aravindsreenandanam11@gmail.com>
2024-08-02 12:42:32 +0530

    forget password validation was added

--- src/pages/Forgetpassword.jsx
+++ src/pages/Forgetpassword.jsx
@@ -1,36 +1,30 @@
 import { Link, useNavigate } from "react-router-dom"
-import { useState } from "react";
-import { auth } from '../firebaseConfig'; 
+import React, { useState } from 'react';
+import { auth } from '../firebaseConfig';
 import { sendPasswordResetEmail } from "firebase/auth";
 const Forgetpassword = () => {
     const [email, setEmail] = useState('');
     const [error, setError] = useState('');
     const [message, setMessage] = useState('');
-const navigate=useNavigate()
-    const handleForgetPassword = async () => {
+    const navigate = useNavigate()
+    const handleForgetPassword = async () => {  
         try {
-            await sendPasswordResetEmail(auth, email)
-            .then(res=>{
-                alert('Password reset email sent! Check your inbox.')
-                navigate("/login")
-            })
-
-            
+          // Attempt to send a password reset email
+          await sendPasswordResetEmail(auth, email);
+          alert('Password reset email sent! Check your inbox.');
+          navigate('/login');
         } catch (error) {
-            let errorMessage;
-            switch (error.code) {
-              case 'auth/invalid-email':
-                errorMessage = 'Invalid email address.';
-                break;
-              case 'auth/user-not-found':
-                errorMessage = 'No user found with this email address.';
-                break;
-              default:
-                errorMessage = 'Error sending password reset email.';
-            }
+          // Handle Firebase errors
+          if (error.code === 'auth/invalid-email') {
+            alert('Invalid email address.');
+          } else if (error.code === 'auth/user-not-found') {
+            alert('No user found with this email address.');
+          } else {
+            alert('Error sending password reset email.');
+          }
         }
-
-}
+      };
+    
     return (
         <div className="login-container">
             <div className="login-box">
